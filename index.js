c125d5aba648fed96fe09c10fd9e9d9f67a62b2a
Dallin Crane <dallincrane@gmail.com>
2016-01-15 13:27:43 -0700

    adding session

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var usersCtrl = require('./controllers/users_controller');
 
 var app = express();
 
+app.use(session({
+  secret: 5,
+  saveUninitialized: true,
+  resave: true
+}));
+
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  console.log(req.session);
+  next();
+});
+
+app.post('/cart', function(req, res, next) {
+  if (!req.session.cart) {
+    req.session.cart = [];
+  }
+  req.session.cart.push(req.body);
+  res.json(req.session.cart);
+});
 
 app.get('/users', usersCtrl.index);
 app.get('/users/:id', usersCtrl.show);
